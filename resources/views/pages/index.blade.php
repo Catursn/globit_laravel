4fc74ef4b3df473dfb61f3ae7733011b1fe549aa
Catursn <catursn@gmail.com>
2020-05-27 00:55:36 +0700

    baru index

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,261 @@
+@include('includes.header')
+
+<div class="block-31" style="position: relative;">
+    <div class="owl-carousel loop-block-31 ">
+      <div class="block-30 block-30-sm item" style="background-image: url('images/header.jpg');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-7">
+              <h2 class="heading mb-5">ONE MAN ONE BROTHER ONE NATION </h2>
+              <p style="display: inline-block;"><a href="https://www.youtube.com/watch?v=qBqKaH7K-zA"  data-fancybox class="ftco-play-video d-flex"><span class="play-icon-wrap align-self-center mr-4"><span class="ion-ios-play"></span></span> <span class="align-self-center">Watch Video</span></a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<!-- WHO ARE WE SECTION -->
+<div class="site-section section-counter">
+<div class="container">
+    <div class="row">
+    <div class="text-center">
+        <h2 class="display-4 mb-3">Who Are We?</h2>
+        <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <p class="mb-4"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        <p class="mb-0"><a href="#" class="btn btn-primary px-3 py-2">Learn More</a></p>
+    </div>
+    </div>
+</div>
+</div>
+<!-- END SECTION -->
+
+<!-- LATEST NEWS SECTION-->
+<div class="site-section bg-light">
+<div class="testimony-section">
+<div class="overlay"></div>
+<div class="container">
+    <div class="row mb-5">
+    <div class="col-md-12" style="padding-top:30px;">
+        <h2>Latest News</h2>
+    </div>
+    </div>
+
+    <div class="row">
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
+        <div class="post-entry">
+        <a href="#" class="mb-3 img-wrap">
+            <img src="images/img_4.jpg" alt="Image placeholder" class="img-fluid">
+        </a>
+        <h3><a href="#">Be A Volunteer Today</a></h3>
+        <span class="date mb-4 d-block text-muted">July 26, 2018</span>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+        <p><a href="#" class="link-underline">Read More</a></p>
+        </div>
+    </div>
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
+        <div class="post-entry">
+        <a href="#" class="mb-3 img-wrap">
+            <img src="images/img_5.jpg" alt="Image placeholder" class="img-fluid">
+        </a>
+        <h3><a href="#">You May Save The Life of A Child</a></h3>
+        <span class="date mb-4 d-block text-muted">July 26, 2018</span>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+        <p><a href="#" class="link-underline">Read More</a></p>
+        </div>
+    </div>
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
+        <div class="post-entry">
+        <a href="#" class="mb-3 img-wrap">
+            <img src="images/img_6.jpg" alt="Image placeholder" class="img-fluid">
+        </a>
+        <h3><a href="#">Children That Needs Care</a></h3>
+        <span class="date mb-4 d-block text-muted">July 26, 2018</span>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+        <p><a href="#" class="link-underline">Read More</a></p>
+        </div>
+    </div>
+    </div>
+</div>
+</div> 
+</div>
+</div>
+<!-- END SECTION -->
+
+<!-- UPCOMING EVENTS SECTION -->
+<div class="site-section fund-raisers bg-light">
+<div class="container">
+    <div class="row mb-3 justify-content-center">
+    <div class="col-md-8 text-center">
+        <h2>Upcoming Events</h2>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p><a href="#" class="link-underline">View All Events</a></p>
+    </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+
+    <!-- <div class="row"> -->
+    
+    <div class="col-md-12 block-11">
+        <div class="nonloop-block-11 owl-carousel">
+
+        <div class="card fundraise-item">
+            <a href="#"><img class="card-img-top" src="images/img_1.jpg" alt="Image placeholder"></a>
+            <div class="card-body">
+            <h3 class="card-title"><a href="#">Water Is Life. Clean Water In Urban Area</a></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            </div>
+        </div>
+
+        
+        <div class="card fundraise-item">
+            <a href="#"><img class="card-img-top" src="images/img_7.jpg" alt="Image placeholder"></a>
+            <div class="card-body">
+            <h3 class="card-title"><a href="#">Need Shelter for Children in Africa</a></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        
+        <div class="card fundraise-item">
+            <a href="#"><img class="card-img-top" src="images/img_3.jpg" alt="Image placeholder"></a>
+            <div class="card-body">
+            <h3 class="card-title"><a href="#">Children Needs Education</a></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+
+
+        <div class="card fundraise-item">
+            <a href="#"><img class="card-img-top" src="images/img_4.jpg" alt="Image placeholder"></a>
+            <div class="card-body">
+            <h3 class="card-title"><a href="#">Refugees Needs Food</a></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+
+        
+        <div class="card fundraise-item">
+            <a href="#"><img class="card-img-top" src="images/img_6.jpg" alt="Image placeholder"></a>
+            <div class="card-body">
+            <h3 class="card-title"><a href="#">Voluteer </a></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        
+        <div class="card fundraise-item">
+            <a href="#"><img class="card-img-top" src="images/img_3.jpg" alt="Image placeholder"></a>
+            <div class="card-body">
+            <h3 class="card-title"><a href="#">Children Needs Food</a></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+        </div>
+        
+        </div>
+    </div>
+    <!-- </div> -->
+</div>
+</div>
+ <!-- END SECTION -->
+
+<!-- FEEDBACK SECTION -->
+<div class="site-section fund-raisers">
+    <div class="testimony-section">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row mb-3 justify-content-center">
+            <div class="col-md-8 text-center" style="padding-top:30px;">
+            <h2>Donatur Feedbacks</h2>
+            <p class="lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="mb-5"><a href="#" class="link-underline">View All Donations</a></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-3 mb-5">
+            <div class="person-donate text-center">
+                <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid">
+                <div class="donate-info">
+                <h2>Christine Charles</h2>
+                <p> </p>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+            </div>  
+            </div>
+
+            <div class="col-md-6 col-lg-3 mb-5">
+            <div class="person-donate text-center">
+                <img src="images/person_2.jpg" alt="Image placeholder" class="img-fluid">
+                <div class="donate-info">
+                <h2>Christine Charles</h2>
+                <p> </p>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+            </div>    
+            </div>    
+
+            <div class="col-md-6 col-lg-3 mb-5">
+            <div class="person-donate text-center">
+                <img src="images/person_3.jpg" alt="Image placeholder" class="img-fluid">
+                <div class="donate-info">
+                <h2>Christine Charles</h2>
+                <p> </p>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+            </div>    
+            </div> 
+        
+            <div class="col-md-6 col-lg-3 mb-5">
+            <div class="person-donate text-center">
+                <img src="images/person_4.jpg" alt="Image placeholder" class="img-fluid">
+                <div class="donate-info">
+                <h2>Christine Charles</h2>
+                <p> </p>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            </div>    
+            </div>           
+        </div>
+    </div>
+    </div>
+</div> 
+<!-- END SECTION -->  
+
+<!-- VOULENTEER SECTION -->  
+<div class="featured-section overlay-color-2" style="background-image: url('images/bg_2.jpg');">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-6 mb-5 mb-md-0">
+          <img src="images/bg_2.jpg" alt="Image placeholder" class="img-fluid">
+        </div>
+        <div class="col-md-6 pl-md-5">
+            <div class="form-volunteer">            
+                <h2>Be A Volunteer Today</h2>
+                <form action="#" method="post">
+                <div class="form-group">
+                    <!-- <label for="name">Name</label> -->
+                    <input type="text" class="form-control py-2" id="name" placeholder="Enter your name">
+                </div>
+                <div class="form-group">
+                    <!-- <label for="email">Email</label> -->
+                    <input type="text" class="form-control py-2" id="email" placeholder="Enter your email">
+                </div>
+                <div class="form-group">
+                    <!-- <label for="v_message">Email</label> -->
+                    <textarea name="v_message" id="" cols="30" rows="3" class="form-control py-2" placeholder="Write your message"></textarea>
+                    <!-- <input type="text" class="form-control py-2" id="email"> -->
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-white px-5 py-2" value="Send">
+                </div>
+                </form>
+            </div>
+        </div>        
+      </div>
+    </div>
+
+  </div> 
+  <!-- END SECTION -->
+@include('includes.footer')
